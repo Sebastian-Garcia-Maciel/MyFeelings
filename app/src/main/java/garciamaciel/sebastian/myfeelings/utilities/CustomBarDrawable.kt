b222344fd5672461bf2892cccb9a97db6ca6cd95
Sebastián García Maciel <sebastian-maciel@outlook.com>
2021-05-04 22:16:30 -0500

    Se arreglo problema con la clase CustomBarDrawable.kt que impedia que se graficara en los views

--- app/src/main/java/garciamaciel/sebastian/myfeelings/utilities/CustomBarDrawable.kt
+++ app/src/main/java/garciamaciel/sebastian/myfeelings/utilities/CustomBarDrawable.kt
@@ -7,49 +7,51 @@ import androidx.core.content.ContextCompat
 import garciamaciel.sebastian.myfeelings.R
 
 class CustomBarDrawable : Drawable {
+
     var coordenadas: RectF? = null
     var context: Context? = null
     var emocion: Emociones? = null
 
-    constructor(context: Context, emociones: Emociones) {
+    constructor(context: Context, emocion: Emociones) {
         this.context = context
         this.emocion = emocion
     }
 
     override fun draw(canvas: Canvas) {
         val fondo: Paint = Paint()
+
         fondo.style = Paint.Style.FILL
         fondo.isAntiAlias = true
         fondo.color = context?.resources?.getColor(R.color.gray) ?: R.color.gray
+
         val ancho: Float = (canvas.width - 10).toFloat()
         val alto: Float = (canvas.height - 10).toFloat()
 
         coordenadas = RectF(0.0F, 0.0F, ancho, alto)
-
         canvas.drawRect(coordenadas!!, fondo)
 
         if (this.emocion != null) {
 
-            val  porcentaje: Float = this.emocion!!.porcentaje * (canvas.width -10) / 100
-            var coordenada2 = RectF(0.0F, 0.0F, porcentaje, alto)
+            val porcentaje: Float = this.emocion!!.porcentaje * (canvas.width - 10) / 100
+            val coordenadas2 = RectF(0.0F, 0.0F, porcentaje, alto)
+            var seccion: Paint = Paint()
 
-            var seccion:Paint = Paint()
             seccion.style = Paint.Style.FILL
             seccion.isAntiAlias = true
             seccion.color = ContextCompat.getColor(this.context!!, emocion!!.color)
-
-            canvas.drawRect(coordenada2!!, seccion)
+            canvas.drawRect(coordenadas2!!, seccion)
         }
-
     }
 
     override fun setAlpha(alpha: Int) {
-    }
 
-    override fun setColorFilter(colorFilter: ColorFilter?) {
     }
 
     override fun getOpacity(): Int {
         return PixelFormat.OPAQUE
     }
-}
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+
+    }
+} 
